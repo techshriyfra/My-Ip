ea08324a2ea5aa3849a0da308b04f91da444aeff
techshriyfra <techsashif+eufya@gmail.com>
2025-02-21 15:44:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'GreyMatters'
+def home():
+    return "Telegram Bot is Running!"
 
-
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
