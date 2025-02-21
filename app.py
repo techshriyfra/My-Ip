fdfd77afa5edeef75209564d0a1f394d9f272645
techshriyfra <techsashif+eufya@gmail.com>
2025-02-21 15:18:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'GreyMatters'
-
+@app.route("/")
+def home():
+    return "Bot is running!"
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.getenv("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
